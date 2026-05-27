09b030fb0b9e13345639efaa8bfb03e18f4f9f39
William Monteiro dos Santos <william58266546@aluno.ba.senac.br>
2026-05-27 16:52:30 -0300

    atualização geral3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,69 @@
 alert("Bem-vindo!");
+
+//let idade = 20;
+//idade = 21;
+//console.log(idade)
+
+//const cidade = "Fortaleza";
+//const nome = "Ana";
+//nome = "Maria";
+
+//Ex
+//let estado
+//if (true) {
+  //estados = "BA"
+//}
+//console.log(estado)
+
+//Jeito Errado
+//if (true) {
+    //var nome = "João";
+//}
+//console.log(nome);
+
+//Jeito Certo
+//if (true) {
+    //let nome ="João;"
+//}
+//console.log(nome);
+
+//Jeito Errado
+//var nome = "Ana";
+ //nome = "Maria";
+ //console.warn(nome);
+
+//Jeito Certo 
+ //let nomeUser = "Ana";
+  //   nomeUser = "Maria";
+//console.warn(nomeUser);
+
+//Jeito Certo
+//var nome;
+//console.log(nome);
+//nome = "João";
+
+//Jeito Errado
+//console.log(nome);
+//nome = "João";
+
+//Jeito Errado
+//console.log(nome)
+//let nome; = "João"
+
+let nome = prompt("Digite seu nome");
+console.log(nome);
+
+let soma = prompt("Deseja sair");
+console.log(resposta);
+
+//---------------------------------------
+//let numero1 = 10;
+//let numero2 = 5;
+// Operações e saídas no console
+//console.log(numero1 + numero2); // 15
+//console.log(numero1 - numero2); // 5
+//console.log(numero1 * numero2); // 50
+//console.log(numero1 / numero2); // 2
+
+let nome = "João";
+console.log("Ola" + nome);
