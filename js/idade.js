4f7a1f1039c225e2f4664f906ae237be010441ae
William Monteiro dos Santos <william58266546@aluno.ba.senac.br>
2026-05-29 16:33:43 -0300

    atualização geral 0.2

---
+++ js/idade.js
@@ -0,0 +1,4 @@
+//Mini Projeto 1:
+let idade = Number(
+  prompt("Digite sua idade")
+);
--- js/scripts.js
+++ js/scripts.js
@@ -11,30 +11,30 @@ alert("Bem-vindo!");
 //Ex
 //let estado
 //if (true) {
-  //estados = "BA"
+//estados = "BA"
 //}
 //console.log(estado)
 
 //Jeito Errado
 //if (true) {
-    //var nome = "João";
+//var nome = "João";
 //}
 //console.log(nome);
 
 //Jeito Certo
 //if (true) {
-    //let nome ="João;"
+//let nome ="João;"
 //}
 //console.log(nome);
 
 //Jeito Errado
 //var nome = "Ana";
- //nome = "Maria";
- //console.warn(nome);
+//nome = "Maria";
+//console.warn(nome);
 
-//Jeito Certo 
- //let nomeUser = "Ana";
-  //   nomeUser = "Maria";
+//Jeito Certo
+//let nomeUser = "Ana";
+//   nomeUser = "Maria";
 //console.warn(nomeUser);
 
 //Jeito Certo
@@ -50,8 +50,7 @@ alert("Bem-vindo!");
 //console.log(nome)
 //let nome; = "João"
 
-//let nome = prompt("Digite seu nome");
-//console.log(nome);
+console.log(prompt("Digite seu nome"));
 
 //let soma = prompt("Deseja sair");
 //console.log(resposta);
@@ -70,6 +69,7 @@ alert("Bem-vindo!");
 //console.log(n1 + n2);
 
 
+
 //let nome = "João";
 //console.log("Ola" + nome);
 
@@ -77,9 +77,10 @@ alert("Bem-vindo!");
 //console.log(`Olá $(nome)`);
 
 
-// Coleta de dados 
-let nome = print("Seu nome");
-let idade = Number(prompt("Sua idade"))
+
+// Coleta de dados
+let nome = prompt("Seu nome");
+let idade = Number(prompt("Sua idade"));
 
 // Exibição
 alert(`Olá ${nome}`);
@@ -88,3 +89,50 @@ console.log(`Idade: ${idade}`);
 // Cálculo e Saída
 let proximo = idade + 1;
 alert(`Próximo ano: ${proximo}`);
+
+//Mini Projeto 1:
+if (idade >= 18) {
+  alert("Maior de idade");
+} else {
+  alert("Menor de idade");
+}
+
+//Mini Projeto 2:
+let nota = Number(prompt("Digite sua nota"));
+if (nota >= 7) {
+  alert("Aprovado");
+} else if (nota >= 5) {
+  alert("Recuperação");
+} else {
+  alert("Reprovado");
+}
+
+//Mini Projeto 3:
+let numero = Number(prompt("Digite um número"));
+if (numero % 2 === 0) {
+  alert("Número par");
+} else {
+  alert("Número ímpar");
+}
+
+//Mini Projeto 4:
+let n1 = Number(prompt("N1"));
+let n2 = Number(prompt("N2"));
+let operacao = prompt("Digite a operação (+, -, *, /)");
+let resultado;
+
+if (operacao === "+") {
+  alert(n1 + n2);
+}
+else if (operacao === "-") {
+  alert(n1 - n2);
+}
+else if (operacao === "*") {
+  alert(n1 * n2);
+}
+else if (operacao === "/") {
+  alert(n1 / n2);
+}
+else {
+  alert("Inválido");
+}
